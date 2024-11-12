b4774d1759c5a7db7d3272a65e02a5c4f160dc3f
opedrogouveia <pedrogouveia1305@gmail.com>
2024-11-12 11:35:21 -0300

    oAuth2.0

---
+++ aula12-oAuth2.0/login.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const axios = require('axios');
+
+// Base64 encode das credenciais para enviar no cabeçalho de autenticação
+const getBearerToken = async () => {
+    const credentials = `${process.env.API_KEY}:${process.env.API_SECRET_KEY}`;
+    const base64Credentials = Buffer.from(credentials).toString('base64');
+    try {
+        const response = await axios.post('https://api.twitter.com/oauth2/token', 'grant_type=client_credentials', {
+                headers: {
+                    Authorization: `Basic ${base64Credentials}`,
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                }
+            });
+        return response.data.access_token;
+    } catch (error) {
+        console.error('Erro ao obter o Bearer Token:', error);
+    }
+};
+
+// Função para buscar dados usando o Bearer Token
+const fetchTweets = async () => {
+    const bearerToken = await getBearerToken();
+    if (bearerToken) {
+        try {
+            const response = await axios.get('https://api.twitter.com/2/tweets/search/recent?query=javascript', {
+                    headers: {
+                        Authorization: `Bearer ${bearerToken}`
+                    }
+                });
+            console.log('Tweets:', response.data);
+        } catch (error) {
+            console.error('Erro ao buscar tweets:', error);
+        }
+    }
+};
+
+fetchTweets();
